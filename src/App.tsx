2bd41911893618dc0975775f027e403a8692454c
NwobiaDavid <dnwobia@gmail.com>
2023-08-01 08:21:32 +0100

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Heropage from './components/Heropage'
+import Header from './components/header'
+import Navbar from './components/Navbar'
+
+
+function App() {
+
+  return (
+    <>
+   <Header />
+      <h2>hey</h2>
+      <Navbar />
+      <Heropage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="flex">
+      <h1>Email: dnwobia@gmail.com</h1>
+      <h1>Hotline:(+234)9153137550</h1>
+      <h1>chat with us on whatsapp</h1>
+    </div>
+  )
+}
